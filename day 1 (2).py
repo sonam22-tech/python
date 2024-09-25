58b67c8a5514ec1a176b080418290804f350ae46
sonam22-tech <sksonam20092005@gmail.com>
2024-09-26 02:08:20 +0530

    Add files via upload

---
+++ day 1 (2).py
@@ -0,0 +1,62 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello","sonam","how","are","you")
+hello sonam how are you
+
+======================================================= RESTART: C:/Users/skson/AppData/Local/Programs/Python/Python312/day 1.py =======================================================
+hello sonam how are you
+2**3
+8
+4//2
+2
+4.5//4
+1.0
+4.5//2
+2.0
+5//2
+2
+x=5
+>>> x**=2
+>>> x=5
+>>> x**=2
+>>> 
+======================================================= RESTART: C:/Users/skson/AppData/Local/Programs/Python/Python312/day 1.py =======================================================
+hello sonam how are you
+>>> x=["rose","lotus"]
+>>> print('is value present?',"rose"in x)
+is value present? True
+>>> print('is value not present?',"riya"not in x)
+is value not present? True
+>>> print("rose",in y)
+SyntaxError: invalid syntax
+>>> print("lotus",in x)
+SyntaxError: invalid syntax
+>>> x=["rose"."lotus"]
+SyntaxError: invalid syntax
+>>> x=["rose","lotus"]
+>>> print("rose",in x)
+SyntaxError: invalid syntax
+>>> print("rose"in x)
+True
+>>> x=6
+>>> y=5
+>>> print(x is y)
+False
+>>> print(x is  not y)
+True
+>>> y=x
+>>> print(x is y)
+True
+>>> x="sonam"
+>>> y=7
+>>> print(x is not y)
+True
+>>> z="7
+SyntaxError: unterminated string literal (detected at line 1)
+>>> z="7"
+>>> print(y is z)
+False
+>>> False
+False
+>>> 
+>>> # this is comment
